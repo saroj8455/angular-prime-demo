2417a12f9a5a7f24f8b09497944ffb6f9903af1b
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-08-15 07:34:20 +0530

    init setup primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primeangular';
+export class AppComponent implements OnInit  {
+
+  @ViewChild('dateFilter') private dateFilter: any;
+  date: Date | undefined;
+  rangeDates: Date[] =[];
+  text:string | undefined;
+  formGroup: FormGroup<any>;
+
+  public onDatesRangeFilterSelected(selectedValue: Date) {
+    if (this.rangeDates[1]) { // If second date is selected
+        this.dateFilter.hideOverlay()
+    };
+}
+
+constructor(){
+  this.formGroup = new FormGroup({
+    text_2: new FormControl()
+  })
+}
+
+ngOnInit(): void {
+console.log(this.formGroup.get('text'));
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { EditorModule } from 'primeng/editor';
+import { CardModule } from 'primeng/card';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ButtonModule,
+    CalendarModule,
+    EditorModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
